4816e712e0160c81eb6491de41c957585cc1ef99
peterortiz82 <peterortiz82@gmail.com>
2016-03-03 21:46:30 -0500

    made some minor code fixes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,8 @@
 class Pin < ActiveRecord::Base
-     belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
-     do_not_validate_attachment_file_type :image
+  belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
+  do_not_validate_attachment_file_type :image
 
-     validates :image, presence: true
+  validates :image, presence: true
   validates :description, presence: true
 end
